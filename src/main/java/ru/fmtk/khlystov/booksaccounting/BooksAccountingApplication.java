40d79a8763cf13b62c07d6a8906df871966bb662
MaxHlystov <maxvls@gmail.com>
2019-06-19 23:41:42 +0300

    Create empty spring-boot app.

---
+++ src/main/java/ru/fmtk/khlystov/booksaccounting/BooksAccountingApplication.java
@@ -0,0 +1,13 @@
+package ru.fmtk.khlystov.booksaccounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksAccountingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksAccountingApplication.class, args);
+	}
+
+}
