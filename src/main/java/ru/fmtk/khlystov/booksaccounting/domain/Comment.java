5ae1e8951658b95a98682321b327921feb5514a8
MaxHlystov <maxvls@gmail.com>
2019-06-28 14:10:08 +0300

    Added comment to the domain of app.

---
+++ src/main/java/ru/fmtk/khlystov/booksaccounting/domain/Comment.java
@@ -0,0 +1,70 @@
+package ru.fmtk.khlystov.booksaccounting.domain;
+
+import org.jetbrains.annotations.NotNull;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "Comments")
+public class Comment {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private LocalDateTime date;
+
+    public Comment() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Comment genre = (Comment) o;
+        return id == genre.id &&
+                name.equals(genre.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("#%s %s %t",
+                (id == -1) ? "-" : Integer.toString(id),
+                name,
+                date);
+    }
+}
