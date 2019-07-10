09327cbbdcb3c7a2c9b3ba8945ede5b4dc2ca346
MaxHlystov <maxvls@gmail.com>
2019-07-11 00:33:55 +0300

    Add key-value support.

--- src/main/java/ru/fmtk/khlystov/booksaccounting/BooksAccountingApplication.java
+++ src/main/java/ru/fmtk/khlystov/booksaccounting/BooksAccountingApplication.java
@@ -2,8 +2,10 @@ package ru.fmtk.khlystov.booksaccounting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.map.repository.config.EnableMapRepositories;
 
 @SpringBootApplication
+@EnableMapRepositories("ru.fmtk.khlystov.booksaccounting.repository")
 public class BooksAccountingApplication {
 
 	public static void main(String[] args) {
